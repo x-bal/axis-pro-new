095307f7a994e8c18573981e92d7a335087c3b71
qsindoempat <git4@qsindo.com>
2021-09-29 17:10:35 +0700

    Update

--- resources/views/case-list/laporan.blade.php
+++ resources/views/case-list/laporan.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.7/css/fixedHeader.bootstrap.min.css">
@@ -42,12 +42,23 @@
                                 <input type="date" id="to" name="to" value="{{ $to }}" readonly class="form-control">
                             </div>
                         </div>
+                        @if(auth()->user()->hasRole('admin'))
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="adjuster">adjuster</label>
+                                <input type="text" id="adjuster" name="adjuster" value="{{ $adjuster }}" readonly class="form-control">
+                            </div>
+                        </div>
+                        @else
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label for="status">status</label>
                                 <input type="text" id="status" name="status" value="{{ $status }}" readonly class="form-control">
                             </div>
                         </div>
+
+                        @endif
                     </div>
                 </div>
 
@@ -56,113 +67,113 @@
         <br>
         <div class="row">
             <div class="col-md-12">
-                                <div class="table-responsive">
-                <table class="table table-bordered" id="table"  style="font-size: 15px;">
-                    <thead>
-                        <tr>
-                            <th rowspan="2">File No</th>
-                            <th rowspan="2">Insurance</th>
-                            <th rowspan="2">Adjuster</th>
-                            <th rowspan="2">Broker</th>
-                            <th rowspan="2">Incident</th>
-                            <th rowspan="2">Policy</th>
-                            <th rowspan="2">Category</th>
-                            <th rowspan="2">Insured</th>
-                            <th colspan="2" class="text-center">Claim Amount</th>
-                            <th colspan="2" class="text-center">Fee</th>
-                            <th colspan="2" class="text-center">Expense</th>
-                            <th rowspan="2">Instruction Date</th>
-                            <th rowspan="2">Survey Date</th>
-                            <th rowspan="2">Now Update</th>
-                            <th rowspan="2">Ia Date</th>
-                            <th rowspan="2">Ia Amount</th>
-                            <th rowspan="2">Ia Status</th>
-                            <th rowspan="2">Pr Date</th>
-                            <th rowspan="2">Pr amount</th>
-                            <th rowspan="2">Pr Status</th>
-                            <th rowspan="2">Ir Status</th>
-                            <th rowspan="2">Ir St Date</th>
-                            <th rowspan="2">Ir St Amount</th>
-                            <th rowspan="2">Ir St Status</th>
-                            <th rowspan="2">Ir Nd Date</th>
-                            <th rowspan="2">Ir Nd Amount</th>
-                            <th rowspan="2">Ir Nd Status</th>
-                            <th rowspan="2">Pa Date</th>
-                            <th rowspan="2">Pa Amount</th>
-                            <th rowspan="2">Pa Status</th>
-                            <th rowspan="2">Fr Date</th>
-                            <th rowspan="2">Fr Amount</th>
-                            <th rowspan="2">Fr Status</th>
-                            <th rowspan="2">Claim Amount</th>
-                            <th rowspan="2">Fee IDR</th>
-                            <th rowspan="2">Fee USD</th>
-                            <th rowspan="2">WIP IDR</th>
-                            <th rowspan="2">WIP USD</th>
-                            <th rowspan="2">Remark</th>
-                            <th rowspan="2">File Status</th>
-                        </tr>
-                        <tr>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="table" style="font-size: 15px;">
+                        <thead>
+                            <tr>
+                                <th rowspan="2">File No</th>
+                                <th rowspan="2">Insurance</th>
+                                <th rowspan="2">Adjuster</th>
+                                <th rowspan="2">Broker</th>
+                                <th rowspan="2">Incident</th>
+                                <th rowspan="2">Policy</th>
+                                <th rowspan="2">Category</th>
+                                <th rowspan="2">Insured</th>
+                                <th colspan="2" class="text-center">Claim Amount</th>
+                                <th colspan="2" class="text-center">Fee</th>
+                                <th colspan="2" class="text-center">Expense</th>
+                                <th rowspan="2">Instruction Date</th>
+                                <th rowspan="2">Survey Date</th>
+                                <th rowspan="2">Now Update</th>
+                                <th rowspan="2">Ia Date</th>
+                                <th rowspan="2">Ia Amount</th>
+                                <th rowspan="2">Ia Status</th>
+                                <th rowspan="2">Pr Date</th>
+                                <th rowspan="2">Pr amount</th>
+                                <th rowspan="2">Pr Status</th>
+                                <th rowspan="2">Ir Status</th>
+                                <th rowspan="2">Ir St Date</th>
+                                <th rowspan="2">Ir St Amount</th>
+                                <th rowspan="2">Ir St Status</th>
+                                <th rowspan="2">Ir Nd Date</th>
+                                <th rowspan="2">Ir Nd Amount</th>
+                                <th rowspan="2">Ir Nd Status</th>
+                                <th rowspan="2">Pa Date</th>
+                                <th rowspan="2">Pa Amount</th>
+                                <th rowspan="2">Pa Status</th>
+                                <th rowspan="2">Fr Date</th>
+                                <th rowspan="2">Fr Amount</th>
+                                <th rowspan="2">Fr Status</th>
+                                <th rowspan="2">Claim Amount</th>
+                                <th rowspan="2">Fee IDR</th>
+                                <th rowspan="2">Fee USD</th>
+                                <th rowspan="2">WIP IDR</th>
+                                <th rowspan="2">WIP USD</th>
+                                <th rowspan="2">Remark</th>
+                                <th rowspan="2">File Status</th>
+                            </tr>
+                            <tr>
                                 <th>IDR</th>
                                 <th>USD</th>
                                 <th>IDR</th>
                                 <th>USD</th>
                                 <th>IDR</th>
                                 <th>USD</th>
-                        </tr>
-                        
-                    </thead>
-                            @php
-                                $expense_idr = 0;
-                                $expense_usd = 0;
-                            @endphp
-                    <tbody>
-                        @foreach($case as $data)
-                        <tr>
-                            <td>{{ $data->file_no }}</td>
-                            <td>{{ $data->insurance->name }}</td>
-                            <td><strong>{{ $data->adjuster->nama_lengkap }}</strong></td>
-                            <td>{{ $data->broker->nama_broker }}</td>
-                            <td>{{ $data->incident->type_incident }}</td>
-                            <td>{{ $data->policy->type_policy }}</td>
-                            <td>@if($data->category == 1) Marine @else Non Marine @endif</td>
-                            <td>{{ $data->insured }}</td>
-                            <td>@if($data->currency == 'RP')<strong>Rp.</strong> {{ number_format($data->claim_amount) }} @endif</td>
+                            </tr>
+
+                        </thead>
+                        @php
+                        $expense_idr = 0;
+                        $expense_usd = 0;
+                        @endphp
+                        <tbody>
+                            @foreach($case as $data)
+                            <tr>
+                                <td>{{ $data->file_no }}</td>
+                                <td>{{ $data->insurance->name }}</td>
+                                <td><strong>{{ $data->adjuster->nama_lengkap }}</strong></td>
+                                <td>{{ $data->broker->nama_broker }}</td>
+                                <td>{{ $data->incident->type_incident }}</td>
+                                <td>{{ $data->policy->type_policy }}</td>
+                                <td>@if($data->category == 1) Marine @else Non Marine @endif</td>
+                                <td>{{ $data->insured }}</td>
+                                <td>@if($data->currency == 'RP')<strong>Rp.</strong> {{ number_format($data->claim_amount) }} @endif</td>
                                 <td>@if($data->currency == 'USD')<i class="fas fa-dollar-sign"></i> {{ number_format($data->claim_amount) }} @endif</td>
                                 <td>@if($data->currency == 'RP')<strong>Rp.</strong> {{ number_format($data->fee_idr) }} @endif</td>
                                 <td>@if($data->currency == 'USD')<i class="fas fa-dollar-sign"></i> {{ number_format($data->fee_usd) }} @endif</td>
-                                <td>@if($data->currency == 'RP')<strong>Rp.</strong> {{ number_format($data->expense->sum('amount')) }}  @php $expense_idr += $data->expense->sum('amount') @endphp @endif</td>
-                                <td>@if($data->currency == 'USD')<i class="fas fa-dollar-sign"></i> {{ number_format($data->expense->sum('amount')) }}  @php $expense_usd += $data->expense->sum('amount') @endphp @endif</td>
-                            <td>{{ $data->instruction_date }}</td>
-                            <td>{{ $data->survey_date }}</td>
-                            <td>{{ $data->now_update }}</td>
-                            <td>{{ $data->ia_date }}</td>
-                            <td>{{ $data->ia_amount }}</td>
-                            <td>{{ $data->ia_status }}</td>
-                            <td>{{ $data->pr_date }}</td>
-                            <td>{{ $data->pr_amount }}</td>
-                            <td>{{ $data->pr_status }}</td>
-                            <td>{{ $data->ir_status }}</td>
-                            <td>{{ $data->ir_st_date }}</td>
-                            <td>{{ $data->ir_st_amount }}</td>
-                            <td>{{ $data->ir_st_status }}</td>
-                            <td>{{ $data->ir_nd_date }}</td>
-                            <td>{{ $data->ir_nd_amount }}</td>
-                            <td>{{ $data->ir_nd_status }}</td>
-                            <td>{{ $data->pa_date }}</td>
-                            <td>{{ $data->pa_amount }}</td>
-                            <td>{{ $data->pa_status }}</td>
-                            <td>{{ $data->fr_date }}</td>
-                            <td>{{ $data->fr_amount }}</td>
-                            <td>{{ $data->fr_status }}</td>
-                            <td>{{ $data->claim_amount }}</td>
-                            <td>{{ $data->fee_idr }}</td>
-                            <td>{{ $data->fee_usd }}</td>
-                            <td>{{ $data->wip_idr }}</td>
-                            <td>{{ $data->wip_usd }}</td>
-                            <td>{{ $data->remark }}</td>
-                            <td>{{ $data->status->nama_status }}</td>
-                        </tr>
-                        @endforeach
+                                <td>@if($data->currency == 'RP')<strong>Rp.</strong> {{ number_format($data->expense->sum('amount')) }} @php $expense_idr += $data->expense->sum('amount') @endphp @endif</td>
+                                <td>@if($data->currency == 'USD')<i class="fas fa-dollar-sign"></i> {{ number_format($data->expense->sum('amount')) }} @php $expense_usd += $data->expense->sum('amount') @endphp @endif</td>
+                                <td>{{ $data->instruction_date }}</td>
+                                <td>{{ $data->survey_date }}</td>
+                                <td>{{ $data->now_update }}</td>
+                                <td>{{ $data->ia_date }}</td>
+                                <td>{{ $data->ia_amount }}</td>
+                                <td>{{ $data->ia_status }}</td>
+                                <td>{{ $data->pr_date }}</td>
+                                <td>{{ $data->pr_amount }}</td>
+                                <td>{{ $data->pr_status }}</td>
+                                <td>{{ $data->ir_status }}</td>
+                                <td>{{ $data->ir_st_date }}</td>
+                                <td>{{ $data->ir_st_amount }}</td>
+                                <td>{{ $data->ir_st_status }}</td>
+                                <td>{{ $data->ir_nd_date }}</td>
+                                <td>{{ $data->ir_nd_amount }}</td>
+                                <td>{{ $data->ir_nd_status }}</td>
+                                <td>{{ $data->pa_date }}</td>
+                                <td>{{ $data->pa_amount }}</td>
+                                <td>{{ $data->pa_status }}</td>
+                                <td>{{ $data->fr_date }}</td>
+                                <td>{{ $data->fr_amount }}</td>
+                                <td>{{ $data->fr_status }}</td>
+                                <td>{{ $data->claim_amount }}</td>
+                                <td>{{ $data->fee_idr }}</td>
+                                <td>{{ $data->fee_usd }}</td>
+                                <td>{{ $data->wip_idr }}</td>
+                                <td>{{ $data->wip_usd }}</td>
+                                <td>{{ $data->remark }}</td>
+                                <td>{{ $data->status->nama_status }}</td>
+                            </tr>
+                            @endforeach
                             <tr>
                                 <td colspan="8">total</td>
                                 <td>{{ number_format($claim_amount_idr) }}</td>
@@ -172,9 +183,9 @@
                                 <td>{{ number_format($expense_idr) }}</td>
                                 <td>{{ number_format($expense_usd) }}</td>
                             </tr>
-                    </tbody>
-                </table>
-            </div>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
