4fdc51b49d49a53a02a71a3fbc720b77f5b571c3
qsindoempat <git4@qsindo.com>
2021-09-29 13:07:02 +0700

    Table

--- resources/views/dashboard/report.blade.php
+++ resources/views/dashboard/report.blade.php
@@ -18,51 +18,55 @@
                         <div>
                             {{ __($title . ' - (Pending)') }}
                         </div>
+                        @can('case-list-create')
+                        <a href="{{ route('case-list.create') }}" class="btn btn-admin"><i class="fas fa-pen"></i> Create</a>
+                        @endif
                     </div>
                     <div class="table-responsive">
                         <table class="table table-bordered table-striped custom-table" width="100%" id="table">
                             <thead style="font-weight: bold;">
                                 <tr>
-                                    <td rowspan="2" class="border" style="text-align: center; align-items: center;">No</td>
-                                    <td rowspan="2" class="border">File No</td>
-                                    <td rowspan="2" class="border">Initial Adj</td>
-                                    <!-- <td colspan="3" class="text-center border-0">Insurance</td> -->
+                                    <td class="border" style="text-align: center; align-items: center;">No</td>
+                                    <td class="border">File No</td>
+                                    <td class="border">Initial Adj</td>
                                     <td class="border">Name</td>
                                     <td class="border">Share</td>
                                     <td class="border">Leader / Member</td>
-                                    <td rowspan="2" class="border">Leader</td>
-                                    <td rowspan="2" class="border">Insured</td>
-                                    <td rowspan="2" class="border">DOL</td>
-                                    <td rowspan="2" class="border">Risk Location / Project</td>
-                                    <td rowspan="2" class="border">Cause of Lost</td>
-                                    <!-- <td rowspan="2" class="border">Claim of Amount</td>
-                            <td rowspan="2" class="border">Instruction Date</td> -->
-                                    <td rowspan="2" class="border">Status</td>
+                                    <td class="border">Leader</td>
+                                    <td class="border">Insured</td>
+                                    <td class="border">DOL</td>
+                                    <td class="border">Risk Location / Project</td>
+                                    <td class="border">Cause of Lost</td>
+                                    <!-- <td class="border">Claim of Amount</td>
+                            <td class="border">Instruction Date</td> -->
+                                    <td class="border">Status</td>
+                                </tr>
+                                <tr>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 @foreach($cases as $case)
                                 <tr>
-                                    <td rowspan="2">{{ $loop->iteration }}</td>
-                                    <td rowspan="2"><a href="{{ route('case-list.show', $case->id) }}">{{ $case->file_no }}</a></td>
-                                    <td rowspan="2">{{ $case->adjuster->kode_adjuster }}</td>
-                                    <td rowspan="2">{{ $case->insurance->name }}</td>
-                                    <td rowspan="2">
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td><a href="{{ route('case-list.show', $case->id) }}">{{ $case->file_no }}</a></td>
+                                    <td>{{ $case->adjuster->kode_adjuster }}</td>
+                                    <td>{{ $case->insurance->name }}</td>
+                                    <td>
                                         @foreach ($case->member as $member)
                                         {{ $member->share }} %
                                         @endforeach
                                     </td>
-                                    <td rowspan="2">
+                                    <td>
                                         @foreach ($case->member as $member)
                                         {{ $member->is_leader == 1 ? 'Leader' : 'Member' }}
                                         @endforeach
                                     </td>
-                                    <td rowspan="2">{{ $case->insured }}</td>
-                                    <td rowspan="2">{{ $case->dol }}</td>
-                                    <td rowspan="2">{{ $case->risk_location }}</td>
-                                    <td rowspan="2">{{ $case->incident->type_incident }}</td>
-                                    <td rowspan="2">{{ $case->status->nama_status }}</td>
+                                    <td>{{ $case->insured }}</td>
+                                    <td>{{ $case->dol }}</td>
+                                    <td>{{ $case->risk_location }}</td>
+                                    <td>{{ $case->incident->type_incident }}</td>
+                                    <td>{{ $case->status->nama_status }}</td>
                                 </tr>
                                 @endforeach
                             </tbody>
