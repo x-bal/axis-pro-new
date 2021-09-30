39776c66612638541784c2667d0a0ceb10a53da6
qsindoempat <git4@qsindo.com>
2021-09-30 10:15:10 +0700

    test email

--- resources/views/emails/reason.blade.php
+++ resources/views/emails/reason.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -28,6 +29,5 @@
             </div>
         </div>
     </div>
-</div>
 </div>
 @endsection
