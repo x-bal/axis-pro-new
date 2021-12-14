37658e7dc945ccaf1609dde0b94001d2e408dfe0
qsindoempat <git4@qsindo.com>
2021-12-14 09:40:10 +0700

    Update File Penunjukkan

--- resources/views/case-list/show.blade.php
+++ resources/views/case-list/show.blade.php
@@ -148,10 +148,10 @@
                                 <tr>
                                     <td>File Penunjukan</td>
                                     <td>:</td>
-                                    <td><a href="{{ route('case-list.penunjukan', $caseList->id) }}" target="_blank" class="btn btn-danger">File Penunjukan</a></td>
+                                    <td><a href="{{ route('caselist.penunjukan', $caseList->id) }}" target="_blank" class="btn btn-danger">File Penunjukan</a></td>
                                     <td>Copy Polis</td>
                                     <td>:</td>
-                                    <td><a href="{{ asset('/storage/'.$caseList->copy_polis) }}" target="_blank" class="btn btn-info">Copy Polis</a></td>
+                                    <td><a href="{{ route('caselist.copyPolice', $caseList->id) }}" target="_blank" class="btn btn-info">Copy Polis</a></td>
                                 </tr>
                             </tbody>
                         </table>
